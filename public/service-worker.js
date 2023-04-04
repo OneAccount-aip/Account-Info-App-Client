2593f4ff0f9c47dff53f3bbd1b1c2e18493bb56c
sungwon-097 <csw2757@gmail.com>
2023-04-04 20:48:06 +0900

    #12-ServiceWorker를 적용

---
+++ public/service-worker.js
@@ -0,0 +1,49 @@
+import axios from "axios";
+
+const CACHE_NAME = 'my-site-cache-v1';
+const urlsToCache = [
+    '/',
+    '/styles/main.css',
+    '/scripts/main.js'
+];
+
+self.addEventListener('install', function(event) {
+    // 캐시 생성 및 파일 저장
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function(cache) {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+self.addEventListener('activate', function(event) {
+    // 이전 버전 캐시 삭제
+    let cacheWhitelist = ['my-site-cache-v1'];
+    event.waitUntil(
+        caches.keys().then(function(cacheNames) {
+            return Promise.all(
+                cacheNames.map(function(cacheName) {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.open('my-cache').then(function(cache) {
+            return cache.match(event.request).then(function(response) {
+                return (
+                    response ||
+                    axios.get(event.request.url).then(function(response) {
+                        cache.put(event.request, response.clone());
+                        return response;
+                    })
+                );
+            });
+        })
+    );
+});
--- public/serviceWorker.js
+++
@@ -1,9 +0,0 @@
-self.addEventListener('install', function(event) {
-    console.log("installed")
-}); // install 이 끝나면 인스톨되었다고 출력.
-
-registerRoute((req) => req.event.request
-        .headers
-        .get('accept')
-        .includes('text/html') //html 파일들을 캐싱
-    ,new cacheFirst());//캐시 퍼스트 전략을 쓴다.
